ccf6772c0d985a6422dd95516901b85318176226
surajshende247 <surajshende247@gmail.com>
2022-03-08 19:47:30 +0530

    add: .env file with PORT config

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const northIndianFood = ['Chole Bhature', 'Biryani', 'Poha', 'Paratha', 'Dal Tadka']
 const southIndianFood = ['Dosa', 'Idli', 'Uttapam', 'Vada', 'Russam', 'Pongal']
