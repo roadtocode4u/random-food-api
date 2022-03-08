e13d9cc6dd63b05c7b42228bf37b30146bca91d0
surajshende247 <surajshende247@gmail.com>
2022-03-08 20:04:49 +0530

    Random food object added with images

--- index.js
+++ index.js
@@ -5,41 +5,48 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const northIndianFood = ['Chole Bhature', 'Biryani', 'Poha', 'Paratha', 'Dal Tadka']
-const southIndianFood = ['Dosa', 'Idli', 'Uttapam', 'Vada', 'Russam', 'Pongal']
+const foodItems = [
+  {
+    id: 1,
+    name: 'Pizza',
+    price: '$10.00',
+    description: 'A delicious pizza',
+    image: 'https://loremflickr.com/320/240/pizza',
+  },
+  {
+    id: 2,
+    name: 'Burger',
+    price: '$5.00',
+    description: 'A delicious burger',
+    image: 'https://loremflickr.com/320/240/burger',
+  },
+  {
+    id: 3,
+    name: 'Cake',
+    price: '$3.00',
+    description: 'A delicious cake',
+    image: 'https://loremflickr.com/320/240/cake',
+  },
+  {
+    id: 4,
+    name: 'Coffee',
+    price: '$2.00',
+    description: 'A delicious coffee',
+    image: 'https://loremflickr.com/320/240/cofee',
+  },
+  {
+    id: 5,
+    name: 'Tea',
+    price: '$1.00',
+    description: 'A delicious tea',
+    image: 'https://loremflickr.com/320/240/tea',
+  }
+]
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/food/random", (req, res) => {
+  const randomItem = foodItems[Math.floor(Math.random() * foodItems.length)];
+  res.send(randomItem);
 })
-
-app.get('/food/north', (req, res)=>{
-  const index = Math.floor(Math.random() * northIndianFood.length);
-  res.send({
-    food: northIndianFood[index],
-    price: Math.floor(Math.random() * 100),
-    category: 'north'
-  });
-})
-
-app.get('/food/south', (req, res)=>{
-  const index = Math.floor(Math.random() * southIndianFood.length);
-  res.send({
-    food: southIndianFood[index],
-    price: Math.floor(Math.random() * 100),
-    category: 'south'
-  });
-})
-
-app.get('/food/mix', (req, res)=>{
-  const combinedFood = northIndianFood.concat(southIndianFood);
-  const index = Math.floor(Math.random() * combinedFood.length);
-  res.send({
-    food: combinedFood[index],
-    price: Math.floor(Math.random() * 100),
-    category: 'mix'
-  });
-})
-
 
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`);
