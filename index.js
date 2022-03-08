c756bac3c2898047d961506846d92ea7bc435071
surajshende247 <surajshende247@gmail.com>
2022-03-08 20:41:45 +0530

    Cors policy added

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 const foodItems = [
   {
     id: 1,
